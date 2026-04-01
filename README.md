4bd8f96e10291b5f38f58dd0994a5267e9419896
Júlia Gomes Reis <juliagreis.ti@gmail.com>
2026-04-01 10:31:07 -0300

    adicionando conexao e registro

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # programacao_com_sockets
 Trabalho 1 da disciplina de Redes de computadores da UFV
+
+# Ideia do programa:
+
+Em vez de criar um sistema cliente-servidor tradicional, onde todas as mensagens de texto passam por um servidor central, a ideia é construir uma rede P2P. Ou seja, o papel do servidor central é fornecer informação sobre o estado dos usuários da rede e como estabelecer uma conexão com eles. Dessa forma, quando um usuário deseja se conectar com outro, ele deve pedir ao servidor central qual é o endereço de IP e a porta daquele usuário pelo comando ADDR <nome>. Nesse momento, o contato com o servidor é matido apenas para manter o status do usuário, mas é o programa do cliente que estabelece uma conexão TCP direta com o peer escolhido.
---
+++ cliente.py
@@ -0,0 +1,48 @@
+import socket
+
+HOSTP2P='0.0.0.0'
+HOST_SERVIDOR_CENTRAL='200.235.131.66'
+PORTA_SERVIDOR_CENTRAL='10000'
+
+def main():
+
+    #--------INICIALIZAÇÃO DO CLIENTE---------
+    print("Iniciando cliente...")
+
+    #Criar socket TCP
+    p2p_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #Faz o bind na porta 0, deixando para o SO escolher uma porta livre
+    p2p_socket.bind((HOSTP2P,0))
+    #Descobre qual IP e PORTA o SO atribuiu: retorno (ip,porta)
+    myIP,myPORTA=p2p_socket=p2p_socket.getsockname()
+
+    print(f"Cliente IP:{myIP} pronto para estabelecer conexões P2P na porta {myPORTA}.")
+
+    #Pedir nome de usuário (não pode conter ':')
+    nomeUsuario=input ("Digite seu nome de usuário: ")
+
+    #---------Conectar no servidor central--------
+    #Cria um socket exclusivamente para falar com o servidor central
+    servidor_central_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        #Tenta estabelecer a conexão TCP
+        servidor_central_socket.connect((HOST_SERVIDOR_CENTRAL, PORTA_SERVIDOR_CENTRAL))
+        print("-- Conectado ao servidor central! --")
+
+        #Monta mensagem de registro -> função USER
+        msg_registro = f"USER {nomeUsuario}:{myPORTA}\r\n" #\r\n especificado no PDF
+
+        #Enviar registro ao servidor central
+            #obs: o socket trasmite bytes, por isso deve-se usar o .encode()
+        servidor_central_socket.send(msg_registro.encode('utf-8'))
+        print(f"-- Registro enviado ao servidor central: {msg_registro}")
+
+    except Exception as e:
+        print("Erro ao conectar ao servidor central: ",e)
+        return
+
+    #--------Enviar o comando USER com a minha porta, registrando usuário como disponível----
+    #--------Iniciar as Threads (para o KEEP, para escutar peers, etc.)--------
+
+if __name__ == "__main__":
+    main()
